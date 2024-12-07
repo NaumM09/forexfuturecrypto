9efb2c44ebe55510a498e6471d596e621f859c4d
Naum <mahlatse.modiba@gmail.com>
2024-12-07 17:38:11 +0200

    social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaTiktok, FaInstagram, FaTwitter } from 'react-icons/fa'; // Importing social media icons
 import '../styles/Footer.css'; // For styling
 
 const Footer = () => {
@@ -6,6 +7,32 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-content">
         <p>&copy; 2024 ForexFutureCrypto. All Rights Reserved.</p>
+        <div className="social-icons">
+          <a
+            href="https://www.tiktok.com/@fxfuturescrypto" 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link"
+          >
+            <FaTiktok className="social-icon" />
+          </a>
+          <a
+            href="https://www.instagram.com/fxfuturescrypto/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link"
+          >
+            <FaInstagram className="social-icon" />
+          </a>
+          <a
+            href="https://x.com/FXFuturesCrypto"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-link"
+          >
+            <FaTwitter className="social-icon" />
+          </a>
+        </div>
       </div>
     </div>
   );
