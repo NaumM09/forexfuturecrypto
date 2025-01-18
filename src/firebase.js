b3552b8f948b05c0e01ca895da50b5d3a08af5a2
Naum <mahlatse.modiba@gmail.com>
2025-01-18 14:47:39 +0200

    firebase api

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,15 @@ import { getAuth } from "firebase/auth"; // Authentication
 import { getStorage } from "firebase/storage"; // Storage
 import { getFirestore } from "firebase/firestore"; // Firestore
 
-// Your Firebase configuration
+// Your Firebase configuration from environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBABoFFDDXgtjqVJpYrH1mrumzsDdZjCFA",
-  authDomain: "mentorship-851db.firebaseapp.com",
-  projectId: "mentorship-851db",
-  storageBucket: "mentorship-851db.appspot.com",
-  messagingSenderId: "302864406450",
-  appId: "1:302864406450:web:b83a4ef27500ab4272f6ab",
-  measurementId: "G-J3FNYYXP2W",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
